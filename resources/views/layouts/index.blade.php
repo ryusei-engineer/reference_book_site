8f3a393b690b87a4b5cf1a3f6e6e7219dc101938
ryusei <ryusei.engineer@gmail.com>
2024-06-29 17:34:43 +0900

    トップページと詳細ページ

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title','reference_book_site')</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<header>
+  <h1>参考書口コミアプリ</h1>
+</header>
+  @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+Route::get('/reference_book1', function () {
+    return view('reference_book1');
+})->name('reference_book1');
